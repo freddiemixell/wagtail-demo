7d3e01b2176bedc297f198e55e2632f83765282b
Frederick Mixell <frederick.mixell@leftfieldlabs.com>
2023-06-03 10:12:09 -0400

    setting up a few basic blocks

--- home/models.py
+++ home/models.py
@@ -2,40 +2,48 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
+
+from streams import blocks
 
 
 class HomePage(Page):
     lead_text = models.CharField(
         max_length=140,
         blank=True,
-        help_text='Subheading under the banner title',
-    )
+        help_text='Subheading under the banner title')
+
     button = models.ForeignKey(
         'wagtailcore.Page',
         blank=True,
         null=True,
         related_name='+',
         help_text='Select an optional page to link to.',
-        on_delete=models.SET_NULL
-    )
+        on_delete=models.SET_NULL)
+
     button_text = models.CharField(
         max_length=50,
         default='Read more',
         blank=False,
-        help_text='Button text'
-    )
+        help_text='Button text')
+
     banner_background_image = models.ForeignKey(
         'wagtailimages.Image',
         blank=False,
         null=True,
         related_name='+',
         help_text='The banner background image.',
-        on_delete=models.SET_NULL
-    )
+        on_delete=models.SET_NULL)
+
+    body = StreamField([
+            ('title', blocks.TitleBlock()),
+            ('cards', blocks.CardsBlock())
+        ],
+        use_json_field=True, null=True, blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('lead_text'),
         FieldPanel('button'),
         FieldPanel('button_text'),
         FieldPanel('banner_background_image'),
-    ]
+        FieldPanel('body')]
---
+++ streams/blocks.py
@@ -0,0 +1,31 @@
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
+
+class TitleBlock(blocks.StructBlock):
+    
+    text = blocks.CharBlock(
+        required=True,
+        help_text='Text to display'
+    )
+
+    class Meta:
+        template = 'streams/title_block.html'
+        icon = 'edit'
+        label = 'Title'
+        help_text = 'Centered text to display on the page.'
+
+    
+class CardsBlock(blocks.StructBlock):
+
+    cards = blocks.ListBlock(
+        blocks.StructBlock([
+            ('title', blocks.CharBlock(max_length=100, help_text='Bold title text for this card. Max length of 100 characters.')),
+            ('text', blocks.CharBlock(max_length=255, help_text='Optional text for this card. Max length is 255 characters.', required=False)),
+            ('image', ImageChooserBlock(help_text='Image will be automagically cropped to 570px by 370px.'))
+        ])
+    )
+
+    class Meta:
+        template = 'streams/card_block.html'
+        icon = 'image'
+        label = 'Standard Cards'
