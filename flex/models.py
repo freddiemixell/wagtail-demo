dd2e05e64e9fc389869d56efd6fbd72894d8ee88
Frederick Mixell <frederick.mixell@leftfieldlabs.com>
2023-06-05 08:39:58 -0400

    adding blocks to flex pages

--- flex/models.py
+++ flex/models.py
@@ -1,6 +1,32 @@
 from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail.snippets.blocks import SnippetChooserBlock
+from wagtail.admin.panels import FieldPanel
+from wagtail import blocks as wagtail_blocks
+
+from streams import blocks
 
 class FlexPage(Page):
+
+    body = StreamField([
+            ('title', blocks.TitleBlock()),
+            ('cards', blocks.CardsBlock()),
+            ('image_and_text', blocks.ImageAndTextBlock()),
+            ('call_to_action', blocks.CallToActionBlock()),
+            ('testimonial', SnippetChooserBlock(
+                target_model='testimonials.Testimonial',
+                template='streams/testimonial_block.html'
+            )),
+            ('pricing_table', blocks.PricingTableBlock()),
+            ('rich_text', wagtail_blocks.RichTextBlock(
+                template='streams/simple_richtext_block.html',
+                features=['bold', 'italic', 'ol', 'ul', 'link']
+            ))
+        ],
+        use_json_field=True, null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body')]
     
     class Meta:
         verbose_name = 'Flex (misc) page'
